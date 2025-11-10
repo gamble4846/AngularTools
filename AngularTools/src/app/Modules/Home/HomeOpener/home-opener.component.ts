8a96ad1c779b82aaafdbe04f581fa6231772ac79
rohanpatelsatva <rohanp@satvasolutions.com>
2025-11-10 18:51:05 +0530

    19

--- AngularTools/src/app/Modules/Home/HomeOpener/home-opener.component.ts
+++ AngularTools/src/app/Modules/Home/HomeOpener/home-opener.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home-opener',
-  templateUrl: './home-opener.component.html',
-  styleUrls: ['./home-opener.component.css']
+    selector: 'app-home-opener',
+    templateUrl: './home-opener.component.html',
+    styleUrls: ['./home-opener.component.css'],
+    standalone: false
 })
 export class HomeOpenerComponent implements OnInit {
 
